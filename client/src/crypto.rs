a2bf4e583cebf1860928c1382b859ffd2b108b57
RoscoeEH <100191436+RoscoeEH@users.noreply.github.com>
2024-09-12 21:07:43 -0700

    Fixed depreciated warnings using TryFrom

--- client/src/crypto.rs
+++ client/src/crypto.rs
@@ -1,6 +1,5 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
-#[allow(non_snake_case)]
 
 
 use hex_literal::hex;
@@ -8,7 +7,7 @@ use pbkdf2::{pbkdf2_hmac, pbkdf2_hmac_array};
 use sha2::{Sha256, Digest};
 use std::io;
 use std::error::Error;
-
+use std::convert::TryFrom;
 
 use aes_gcm::{aead::{Aead, AeadCore, KeyInit, OsRng}, Aes256Gcm, Nonce, Key};
 
@@ -31,9 +30,9 @@ pub fn key_derivation(password: String) -> [u8;32]{
 pub fn encrypt(message: String, key: [u8; 32]) -> Vec<u8>{
 
     
-    let useKey = Key::<Aes256Gcm>::from_slice(&key);
+    let use_key = Key::<Aes256Gcm>::try_from(key).expect("Invalid key length");
 
-    let cipher = Aes256Gcm::new(&useKey);
+    let cipher = Aes256Gcm::new(&use_key);
     let nonce = Aes256Gcm::generate_nonce().unwrap(); // 96-bits; unique per message
 
     let ciphertext = cipher.encrypt(&nonce, message.as_bytes().as_ref());
@@ -42,13 +41,13 @@ pub fn encrypt(message: String, key: [u8; 32]) -> Vec<u8>{
 }
 
 
-pub fn decrypt(ciphertext: Vec<u8>, aesKey: [u8;32]) -> Vec<u8> {
+pub fn decrypt(ciphertext: Vec<u8>, aes_key: [u8;32]) -> Vec<u8> {
     
     // Extract nonce and ciphertext
     let (nonce, data) = ciphertext.split_at(12);
-    let key = Key::<Aes256Gcm>::from_slice(&aesKey);
+    let key = Key::<Aes256Gcm>::try_from(aes_key).expect("Invalid key length");
     let cipher = Aes256Gcm::new(&key);
- 
+    
     let plaintext = cipher.decrypt(Nonce::from_slice(nonce), data);
 
     plaintext.expect("Decryption Error")
@@ -69,6 +68,6 @@ pub fn hash(input: HashInputType) -> [u8;32] {
 
 // Generates new passwords
 // pub fn generate_password(length: u8, excludeChars: Vec<char>, useWords: bool, charMapping: bool) {
-    
+
 // }
- 
+
